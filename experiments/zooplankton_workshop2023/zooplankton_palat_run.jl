a095f277e3fed9decdf13ed7d3d4deb120394323
Barbie <barbara.raven42@gmail.com>
2023-04-25 14:09:37 -0400

    added strict zoo run typo

--- experiments/zooplankton_workshop2023/zooplankton_palat_run.jl
+++ experiments/zooplankton_workshop2023/zooplankton_palat_run.jl
@@ -1,6 +1,7 @@
 include("../../src/DarOneTools.jl")
 using .DarOneTools
 using NCDatasets
+using DelimitedFiles
 
 # set path to MITgcm 
 MITgcm_path[1] = "/dar_one_docker/darwin3" # CHANGE ME (unless using docker)
