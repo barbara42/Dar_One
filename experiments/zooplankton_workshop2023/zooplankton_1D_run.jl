371238a9fc28fd38d9d44aaf849b049e1bf6033a
Barbie <barbara.raven42@gmail.com>
2023-04-26 21:38:56 -0400

    changed base_configuration

--- experiments/zooplankton_workshop2023/zooplankton_1D_run.jl
+++ experiments/zooplankton_workshop2023/zooplankton_1D_run.jl
@@ -5,8 +5,8 @@ using DelimitedFiles
 
 # set path to MITgcm 
 MITgcm_path[1] = "/dar_one_docker/darwin3" # CHANGE ME (unless using docker)
-config_1D = "dar1_1D_31-32-3"
-filexe=joinpath(MITgcm_path[1],"verification",config_1D,"build","mitgcmuv")
+base_configuration = "dar1_1D_31-32-3"
+#filexe=joinpath(MITgcm_path[1],"verification",config_1D,"build","mitgcmuv")
 
 # set up size of your grid 
 nX = 1
