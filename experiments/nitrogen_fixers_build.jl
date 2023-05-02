3781f6f0daaa60401bac7fe8b395c29e59831a6d
Barbie <barbara.raven42@gmail.com>
2023-05-02 12:31:53 -0400

    changed n fixers build back to 30 x 30

--- experiments/nitrogen_fixers_build.jl
+++ experiments/nitrogen_fixers_build.jl
@@ -6,8 +6,8 @@ using NCDatasets
 MITgcm_path[1] = "/dar_one_docker/darwin3" # CHANGE ME (unless using docker)
 
 # set up size of your grid 
-nX = 1
-nY = 1
+nX = 30
+nY = 30
 
 # update SIZE.h 
 update_grid_size(nX, nY)
